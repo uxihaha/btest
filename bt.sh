0c3ad7ce7f0a993e01e2fc9732dd78ac29eb6124
uxihaha <100551233+uxihaha@users.noreply.github.com>
2022-06-15 16:33:13 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -1,5 +1,5 @@
 wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
-wget -O btpanel.zip http://download.bt.cn/install/update/LinuxPanel-7.7.0.zip && unzip btpanel.zip
-cd btpanel
+wget http://download.bt.cn/install/update/LinuxPanel-7.7.0.zip && unzip LinuxPanel-7.7.0.zip
+cd LinuxPanel-7.7.0.zip/panel
 bash update.sh
 rm -rf  /www/server/panel/data/bind.pl
