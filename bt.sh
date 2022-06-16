7712be1d796379abe152508721e34038721616f8
uxihaha <100551233+uxihaha@users.noreply.github.com>
2022-06-16 09:23:53 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -2,5 +2,5 @@ wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo b
 wget http://download.bt.cn/install/update/LinuxPanel-7.7.0.zip && unzip LinuxPanel-7.7.0.zip && rm LinuxPanel-7.7.0.zip
 cd LinuxPanel-7.7.0/panel
 bash update.sh
-rm -rf  /www/server/panel/data/bind.pl
+sed -i "s|bind_user == 'True'|bind_user == 'XXXX'|" /www/server/panel/BTPanel/static/js/index.js
 bt
