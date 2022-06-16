2131f88e6fefbe6c098a4813571fe456cfc6ad26
uxihaha <100551233+uxihaha@users.noreply.github.com>
2022-06-16 09:57:04 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -2,5 +2,5 @@ wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo b
 wget http://download.bt.cn/install/update/LinuxPanel-7.7.0.zip && unzip LinuxPanel-7.7.0.zip && rm LinuxPanel-7.7.0.zip
 cd LinuxPanel-7.7.0/panel
 bash update.sh
-sed -i "s|bind_user == 'True'|bind_user == 'XXXX'|" /www/server/panel/BTPanel/static/js/index.js
+sed -i “s|if (bind_user == ‘True’) {|if (bind_user == ‘REMOVED’) {|g” /www/server/panel/BTPanel/static/js/index.js
 bt
