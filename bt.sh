110a05cc86e4aff509b551be8a34c6261b2429a2
uxihaha <100551233+uxihaha@users.noreply.github.com>
2022-06-21 10:06:29 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -2,6 +2,5 @@ wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo b
 unzip LinuxPanel-7.7.0.zip && rm LinuxPanel-7.7.0.zip
 cd panel
 bash update.sh
-sed -i “s|if (bind_user == ‘True’) {|if (bind_user == ‘REMOVED’) {|g” /www/server/panel/BTPanel/static/js/index.js
 rm -rf  /www/server/panel/data/bind.pl
 bt 14
