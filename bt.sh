59a3fbad6d92aecb5e4b82790ceda4235ab45132
uxihaha <giftalisce@protonmail.com>
2022-06-21 01:41:33 +0000

    add zip

--- bt.sh
+++ bt.sh
@@ -1,6 +1,7 @@
 wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
-wget http://download.bt.cn/install/update/LinuxPanel-7.7.0.zip && unzip LinuxPanel-7.7.0.zip && rm LinuxPanel-7.7.0.zip
-cd LinuxPanel-7.7.0/panel
+unzip LinuxPanel-7.7.0.zip && rm LinuxPanel-7.7.0.zip
+cd panel
 bash update.sh
 sed -i “s|if (bind_user == ‘True’) {|if (bind_user == ‘REMOVED’) {|g” /www/server/panel/BTPanel/static/js/index.js
-bt
+rm -rf  /www/server/panel/data/bind.pl
+bt 14
