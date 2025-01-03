46bd5101ad3645bd6f48bb5c05bcd94e1e2242ce
uxihaha <100551233+uxihaha@users.noreply.github.com>
2025-01-03 10:57:33 +0800

    Update bt.sh
    因为7.7.0降级包失效，无法安装报错。另外寻找7.6.0包安装替代

--- bt.sh
+++ bt.sh
@@ -1,5 +1,5 @@
 wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
-unzip LinuxPanel-7.7.0.zip && rm LinuxPanel-7.7.0.zip
+unzip LinuxPanel-7.6.0.zip && rm LinuxPanel-7.6.0.zip
 cd panel
 bash update.sh
 rm -rf  /www/server/panel/data/bind.pl
